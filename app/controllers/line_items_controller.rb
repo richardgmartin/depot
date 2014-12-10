ba7eb4dcd8969069fc79356a157f25f536290582
Richard Martin <richardgeorgemartin@gmail.com>
2014-12-10 18:06:35 -0500

    Iteration E3 finishing the cart up to page 132

--- app/controllers/line_items_controller.rb
+++ app/controllers/line_items_controller.rb
@@ -31,11 +31,13 @@ class LineItemsController < ApplicationController
 
     respond_to do |format|
       if @line_item.save
-        format.html { redirect_to @line_item.cart, notice: 'Line item was successfully created.' }
-        format.json { render :show, status: :created, location: @line_item }
+        format.html { redirect_to @line_item.cart }
+        format.json { render action: 'show',
+          status: :created, location: @line_item }
       else
-        format.html { render :new }
-        format.json { render json: @line_item.errors, status: :unprocessable_entity }
+        format.html { render action: 'new' }
+        format.json { render json: @line_item.errors,
+          status: :unprocessable_entity }
       end
     end
   end
