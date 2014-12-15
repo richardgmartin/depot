bd25b0f62dea4bcc33cc0c48ddc887f2392fc9fa
Richard Martin <richardgeorgemartin@gmail.com>
2014-12-15 16:45:34 -0500

    Iteration I3 limiting access up to page 204

--- app/controllers/carts_controller.rb
+++ app/controllers/carts_controller.rb
@@ -1,4 +1,5 @@
 class CartsController < ApplicationController
+  skip_before_action :authorize, only: [:create, :update, :destroy]
   before_action :set_cart, only: [:show, :edit, :update, :destroy]
   rescue_from ActiveRecord::RecordNotFound, with: :invalid_cart
 
@@ -78,5 +79,10 @@ class CartsController < ApplicationController
     # Never trust parameters from the scary internet, only allow the white list through.
     def cart_params
       params[:cart]
+    end
+
+    def invalid_cart
+      logger.error "Attemp to access invalid cart #{params[:id]}"
+      redirect_to store_url, notice: "Invalid cart"
     end
 end
