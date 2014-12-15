89c1320e271688946d4de9d7bfc32056f9d1f94f
Richard Martin <richardgeorgemartin@gmail.com>
2014-12-15 16:20:35 -0500

    Iteration I2 authenticating users up to page 202

--- config/routes.rb
+++ config/routes.rb
@@ -1,15 +1,23 @@
 Rails.application.routes.draw do
+  get 'admin' => 'admin#index'
+  controller :sessions do
+    get 'login' => :new
+    post 'login' => :create
+    delete 'logout' => :destroy
+  end
+
+
+  get 'sessions/create'
+  get 'sessions/destroy'
   resources :users
-
   resources :orders
-
   resources :line_items
-
   resources :carts
 
   get 'store/index'
-
-  resources :products
+    resources :products do
+      get :who_bought, on: :member
+    end
 
   root 'store#index', as: 'store'
 
